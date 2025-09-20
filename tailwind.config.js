ba0b4066b0e3db9de26ec5d220aea2872501ef43
Jon <>
2025-09-20 15:17:31 -0400

    proper tailwind support, WIP on issues page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,vue,html}"
+    "./src/**/*.{js,ts,jsx,tsx,vue,html}",
+    "./**/*.html",
+    "./partials/**/*.html"
   ],
   theme: {
     extend: {},
